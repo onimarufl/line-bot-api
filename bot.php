8821f923c7994a64e9fcb7f7e8a1d8d148d7996e
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-01-26 13:20:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,10 +2,12 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('line-bot-api/php/line-bot.php');
+include ('php/line-bot.php');
 
-$channelSecret = 'f0ee00bdba1460f67f9e0b4991422097';
-$access_token  = 'ozGfNAi/Ho8XhNXyAbvshLXFM5E5u8PFFYuDwcDgHo7BGyVwlDl60J5MoU3zqsflgCNpL9KJl5I6YfMpmNveUjbwcoi4f943KMjpHwmxb+otuhVR7dplahDrd8dHUXI+D8lDXZ1wqLyRiZ6sa+fF3AdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'f0ee00bdba1460f67f9e0b4991422097
+';
+$access_token  = 'ozGfNAi/Ho8XhNXyAbvshLXFM5E5u8PFFYuDwcDgHo7BGyVwlDl60J5MoU3zqsflgCNpL9KJl5I6YfMpmNveUjbwcoi4f943KMjpHwmxb+otuhVR7dplahDrd8dHUXI+D8lDXZ1wqLyRiZ6sa+fF3AdB04t89/1O/w1cDnyilFU=
+';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
