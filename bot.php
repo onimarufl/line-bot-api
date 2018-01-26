aa5dfb848192826a9892548c866643c8acf715f7
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-01-26 12:59:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('php/line-bot.php');
+include ('line-bot-api/php/line-bot.php');
 
 $channelSecret = 'a1e4e88d6473b48c6d2692042a98c15e';
 $access_token  = '9B9ffZ7XJ/iWMWgJuqRV/oaVVMfELLEMmBjoIhqG9E5xdFvOHDNpiZBDjdi2deqYm4SdFCezBQjddXs1EgjLXmCJgBorihv3bfwUxW8zMCoT9EqBEs5CW6wnsUqEoJcKTGPyYznGlnG293DicIlZowdB04t89/1O/w1cDnyilFU=';
